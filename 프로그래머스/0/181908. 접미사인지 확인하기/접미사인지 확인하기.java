38dd6ae586cb4d50d6d733aa7cdb25c1353b6301
고성민 <s2619@e-mirim.hs.kr>
2026-09-06 07:23:29 +0900

    전체 코드 업로드 -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        char a=' ';
+        for(int i=0;i<my_string.length();i++){
+            if(!my_string.substring(i).equals(is_suffix)){
+                a=my_string.charAt(i);
+            } else{
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
